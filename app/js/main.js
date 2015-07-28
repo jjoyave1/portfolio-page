7f48cb8f2ec1195ecd0250154e6b18b5810cb238
Joe Joyave <jjoyave1@gmail.com>
2015-07-28 16:55:33 -0400

    finished styling

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$('.twit').on('mouseenter', function () {
+  $('.twitlk').slideDown();
+  $(this).on('mouseleave', function () {
+    $('.twitlk').fadeOut(1200);
+  });
+});
+
+$('.gith').on('mouseenter', function () {
+  $('.githlk').slideDown();
+  $(this).on('mouseleave', function () {
+    $('.githlk').fadeOut(1200);
+  });
+});
+
+$('.medi').on('mouseenter', function () {
+  $('.medilk').slideDown();
+  $(this).on('mouseleave', function () {
+    $('.medilk').fadeOut(1200);
+  });
+});
+
+$('.lkin').on('mouseenter', function () {
+  $('.lkinlk').slideDown();
+  $(this).on('mouseleave', function () {
+    $('.lkinlk').fadeOut(1200);
+  });
+});
